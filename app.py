8b5e2bbf56d691276371260ca1777ef088e53313
Mishal <mishgit95@gmail.com>
2017-11-05 16:50:04 +0530

    Changed root endpoint

--- app.py
+++ app.py
@@ -2,16 +2,12 @@ from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
 
-@app.route('/login')
+@app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+    <h1>Hello this is the main page of our API</h1>
+    """
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
