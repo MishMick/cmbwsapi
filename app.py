fd239d9380e6a2370dcea7d8b4291d84e41083d9
Mishal <mishgit95@gmail.com>
2017-11-05 17:56:18 +0530

    Added code for pymongo

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+# Use a database already created on mongolab 
+server = 'ds149335.mlab.com'
+port = 41875
+db_name = 'cmbwsdb'
+username = 'ishan'
+password = 'ishan'
+
 from flask import Flask
 from datetime import datetime
+from pymongo import MongoClient
+from pymongo import Connection
 
 app = Flask(__name__)
 
@@ -9,6 +18,24 @@ def homepage():
     return """
     <h1>Hello this is the main page of our API</h1>
     """
+@app.route('/login')
+def loginpage():
+
+# connect to server
+print '\nConnecting ...'
+conn = Connection(server, port)
+
+# Get the database
+print '\nGetting database ...'
+db = conn[db_name]
+
+# Have to authenticate to get access
+print '\nAuthenticating ...'
+db.authenticate(username, password)
+
+	return """
+	<h1>Login page</h1>
+	"""
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
