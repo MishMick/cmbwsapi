769d6859780ab9c628651f4c5e327c8690b6156e
Mishal <mishgit95@gmail.com>
2017-11-05 18:59:21 +0530

    ug4

--- app.py
+++ app.py
@@ -20,9 +20,43 @@ print ("!!!!!!!!!!!!!!!!!!!!!!!!!")
 db=client.db_name
 print ("Database name "+db)
 
+db=client.cmbwsapi
+usersCollection = db.users
+
 @app.route('/')
-def homepage():
-    return "<h1>Hello this is the main page of our API</h1> "
+def index():
+    return "Women Safety APP"
+
+@app.route('/userExists/<int:psid>', methods=['GET'])
+def userExists(psid):
+    if psid == 1:
+    	return "test"
+    else:
+    	return "ps != 1"
+
+@app.route('/validate/<int:psid>/<string:pwd>', methods=['GET'])
+def validate(psid,pwd):
+
+    data= usersCollection.find_one({'psid': psid})
+    
+    if (data['pwd'][0].encode("utf-8") == pwd):
+        return "True"
+
+    return "False"
+
+
+ 
+@app.route('/register/<int:psid>/<string:pwd>/<string:role>', methods=['POST'])
+def register(psid,pwd,role):
+    user = {
+        'psid' : [psid],
+        'pwd' : [pwd],
+        'role' : [role] 
+    }
+
+    result=usersCollection.insert_one(user)
+    return 'Debug : Created ' + str(result.inserted_id)
+
 
 @app.route('/login')
 def loginpage():
